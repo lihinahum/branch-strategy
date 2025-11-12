0047add1c1115d9b858507ac66506687f229e9f5
Lihi Nahum <lihina4@gmail.com>
2025-11-12 16:45:28 +0200

    solve second conflict on multiply

--- calculator.py
+++ calculator.py
@@ -29,7 +29,6 @@ print("4.Divide")
 # Take input from the user 
 choice = input("Enter choice(1/2/3/4):")
 
-
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
@@ -39,7 +38,5 @@ elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
 elif choice == '3':
    print(num1,"*",num2,"=", multiply(num1,num2))
-
 else:
-   
    print("Invalid input ",choice)
