65413dae31897927ac937d1d4cd64630b268cb93
Lihi Nahum <lihina4@gmail.com>
2025-11-12 16:44:22 +0200

    solve conflict on multiply

--- calculator.py
+++ calculator.py
@@ -12,8 +12,7 @@ def subtract(x, y):
 
 # This function multiplies two numbers
 def multiply(x, y):
-   #dummy implementation
-   return 1920
+   return x * y
 
 # This function divides two numbers
 def divide(x, y):
@@ -38,6 +37,9 @@ if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
 elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
 else:
    
    print("Invalid input ",choice)
